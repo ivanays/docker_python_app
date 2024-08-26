5ba69ee28f1dfa282e688d833a58435294dca6a8
ivanays <ivanays@yandex.ru>
2024-08-26 19:14:13 +0300

    v1.02

--- app.py
+++ app.py
@@ -13,13 +13,22 @@ for path in os.listdir(dir_path):
 print('Total number of files: ', count)
 
 print('Top 10 lagests files (in KB): ')
+
+list_of_files = filter( lambda x: os.path.isfile(os.path.join(dir_path, x)),
+                        os.listdir(dir_path) )
+
+list_of_files = sorted( list_of_files,
+                        key =  lambda x: os.stat(os.path.join(dir_path, x)).st_size)
+
+list_of_files = list(reversed(list_of_files))
 count1 = 0
-for path in os.scandir(dir_path):
-    if count1 <= 10:
-        if path.is_file():
-            file_info = os.stat(path)
-            file_size_kb = file_info.st_size / 1024
-            print(f"{path}: {file_size_kb: .2f} KB")
+
+for file_name in list_of_files:
+    if count1 < 10:
+        file_path = os.path.join(dir_path, file_name)
+        file_size  = os.stat(file_path).st_size
+        file_size_kb = file_size / 1024
+        print(f"{file_path}: {file_size_kb: .2f} KB")
     count1 += 1
 
 
